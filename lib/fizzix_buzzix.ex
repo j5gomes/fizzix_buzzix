1d1a61a6cb87d17871115bcc0ccf111e62371594
João Gomes <joaogomes@significa.co>
2025-07-11 15:45:03 +0100

    converting the file string to an list

--- lib/fizzix_buzzix.ex
+++ lib/fizzix_buzzix.ex
@@ -1,22 +1,21 @@
 defmodule FizzixBuzzix do
-  # Using a case as the other languages use
-  # def build(file_name) do
-  #   case File.read(file_name) do
-  #     {:ok, result} -> result
-  #     {:error, reason} -> reason
-  #   end
-  # end
-
-  # The elixir way - Using an function with pattern matching -
   def build(file_name) do
-    # The usual way
-    # file = File.read(file_name)
-    # handle_file_read(file)
-
-    # The elixir way
-    file_name |> File.read() |> handle_file_read()
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    # An regular anonymous function
+    # |> Enum.map(fn number -> String.to_integer(number) end)
+    # An shortcut for the regular anonymous function
+    # 1. We can pass the first param as &1
+    # |> Enum.map(&String.to_integer(&1))
+    # 2. We can pass the first param as the arity of the function
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 end
