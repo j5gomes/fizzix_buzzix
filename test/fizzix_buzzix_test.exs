c521a80657ff29f1daa8c7b23606a026196bafa8
João Gomes <joaogomes@significa.co>
2025-07-11 16:26:31 +0100

    add tests to the build/1 module

--- test/fizzix_buzzix_test.exs
+++ test/fizzix_buzzix_test.exs
@@ -1,8 +1,19 @@
 defmodule FizzixBuzzixTest do
   use ExUnit.Case
-  doctest FizzixBuzzix
 
-  test "greets the world" do
-    assert FizzixBuzzix.hello() == :banana
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response =
+        {:ok, [1, 2, :fizzix, 4, :buzzix, :fizzix, :buzzix, :fizzixbuzzix, :buzzix]}
+
+      assert FizzixBuzzix.build("numbers.txt") == expected_response
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      expected_response =
+        {:error, "Error reading the file: enoent"}
+
+      assert FizzixBuzzix.build("invalid.txt") == expected_response
+    end
   end
 end
